4805a53daabd26414107b7ceee374d9afba0e185
anggis <anggisdar@gmail.com>
2024-12-09 13:19:46 +0700

    feat : add show controller

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -6,6 +6,7 @@ import (
 	// "github.com/anggisdar/gin-crud-mysql/models"
 	"github.com/anggisdar/gin-crud-mysql/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Index(c *gin.Context) {
@@ -16,12 +17,29 @@ func Index(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"products": products})
 
 }
-func Show(c *gin.Context) {
 
+func Show(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
+
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data tidak ditemukan"})
+			return
+		default:
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"product": product})
 }
+
 func Create(c *gin.Context) {
 
 }
+
 func Update(c *gin.Context) {
 
 }
